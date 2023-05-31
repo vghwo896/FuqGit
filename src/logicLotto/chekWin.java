624e09adbaca7a698e95c7331b6a393feb060f48
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-31 17:00:30 +0900

    구매한 로또 번호 출력

--- src/logicLotto/chekWin.java
+++ src/logicLotto/chekWin.java
@@ -9,6 +9,7 @@ import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,6 +18,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.SpringLayout;
 import javax.swing.JLabel;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
 
 public class chekWin extends JFrame {
@@ -70,37 +72,37 @@ public class chekWin extends JFrame {
 		SpringLayout sl_panel_1 = new SpringLayout();
 		panel_1.setLayout(sl_panel_1);
 
-		JLabel lblNewLabel_5 = new JLabel("당첨공 1\r\n");
+		JLabel lblNewLabel_5 = new JLabel(new ImageIcon(win[0]+".png"));
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_5, 32, SpringLayout.WEST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.SOUTH, lblNewLabel_5, -43, SpringLayout.SOUTH, panel_1);
 		panel_1.add(lblNewLabel_5);
 
-		JLabel lblNewLabel_6 = new JLabel("당첨공 2");
+		JLabel lblNewLabel_6 = new JLabel(new ImageIcon(win[1]+".png"));
 		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_6, 0, SpringLayout.NORTH, lblNewLabel_5);
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_6, 6, SpringLayout.EAST, lblNewLabel_5);
 		panel_1.add(lblNewLabel_6);
 
-		JLabel lblNewLabel_7 = new JLabel("당첨공4");
+		JLabel lblNewLabel_7 = new JLabel(new ImageIcon(win[2]+".png"));
 		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_7, 0, SpringLayout.NORTH, lblNewLabel_5);
 		panel_1.add(lblNewLabel_7);
 
-		JLabel lblNewLabel_8 = new JLabel("당첨공3");
+		JLabel lblNewLabel_8 = new JLabel(new ImageIcon(win[3]+".png"));
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_7, 6, SpringLayout.EAST, lblNewLabel_8);
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_8, 6, SpringLayout.EAST, lblNewLabel_6);
 		sl_panel_1.putConstraint(SpringLayout.SOUTH, lblNewLabel_8, 0, SpringLayout.SOUTH, lblNewLabel_5);
 		panel_1.add(lblNewLabel_8);
 
-		JLabel lblNewLabel_9 = new JLabel("당첨공5");
+		JLabel lblNewLabel_9 = new JLabel(new ImageIcon(win[4]+".png"));
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_9, 6, SpringLayout.EAST, lblNewLabel_7);
 		sl_panel_1.putConstraint(SpringLayout.SOUTH, lblNewLabel_9, 0, SpringLayout.SOUTH, lblNewLabel_5);
 		panel_1.add(lblNewLabel_9);
 
-		JLabel lblNewLabel_10 = new JLabel("당첨공 6");
+		JLabel lblNewLabel_10 = new JLabel(new ImageIcon(win[5]+".png"));
 		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_10, 0, SpringLayout.NORTH, lblNewLabel_5);
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_10, 5, SpringLayout.EAST, lblNewLabel_9);
 		panel_1.add(lblNewLabel_10);
 
-		JLabel lblNewLabel_11 = new JLabel("당첨공 보너스");
+		JLabel lblNewLabel_11 = new JLabel(new ImageIcon(win[6]+".png"));
 		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_11, 0, SpringLayout.NORTH, lblNewLabel_5);
 		sl_panel_1.putConstraint(SpringLayout.EAST, lblNewLabel_11, -35, SpringLayout.EAST, panel_1);
 		panel_1.add(lblNewLabel_11);
@@ -133,8 +135,22 @@ public class chekWin extends JFrame {
 		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblNewLabel, -5, SpringLayout.SOUTH, contentPane);
 		SpringLayout sl_panel = new SpringLayout();
 		panel.setLayout(sl_panel);
-
-		JScrollBar scrollBar = new JScrollBar();
+		
+		JPanel scrpnl = new JPanel();
+		JPanel pnl2 = new JPanel();
+		scrpnl.setLayout(new BoxLayout(scrpnl, BoxLayout.Y_AXIS));
+		
+		for (int i = 1; i <= gen.m.getLottoMap().keySet().size(); i++) {
+			for (int j = 0; j < gen.m.getLottoMap().get(i).getNum().length; j++) {
+				int[] arr = gen.m.getLotto(i);
+				JLabel lbl = new JLabel(new ImageIcon(arr[j]+".png"));
+				pnl2.add(lbl);
+			}
+			scrpnl.add(pnl2);
+		}
+		
+		JScrollPane scrollBar = new JScrollPane();
+		scrollBar.add(scrpnl);
 		sl_panel.putConstraint(SpringLayout.NORTH, scrollBar, 10, SpringLayout.NORTH, panel);
 		sl_panel.putConstraint(SpringLayout.SOUTH, scrollBar, 366, SpringLayout.NORTH, panel);
 		sl_panel.putConstraint(SpringLayout.EAST, scrollBar, -10, SpringLayout.EAST, panel);
